596ae2e1f6481356a86378889a534d3884705192
Ubuntu <ubuntu@ip-172-31-91-231.ec2.internal>
2019-06-23 21:54:10 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,109 @@
+//TIMEOUT
+/*var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open ("GET", "https://swapi.co/api/");
+xhr.send();
+
+
+xhr.onreadystatechange= function () {
+    if (this.readyState == 4 && this.status ==200) {
+      data = JSON.parse(this.responseText);
+    }
+};
+
+setTimeout(function () {
+    console.log(data);
+}, 500);*/
+//CALLBACK To Write to Console
+/*function getData(cb) {
+    var xhr = new XMLHttpRequest();
+
+
+    xhr.open("GET", "https://swapi.co/api/");
+    xhr.send();
+
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb (JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+getData(printDataToConsole);*/
+//To Write to Document Part One
+
+/*const baseURL = "https://swapi.co/api/";
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb (JSON.parse(this.responseText));
+        }
+    };
+}
+function writeToDocument(type) {
+    getData(type, function (data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}*/
+// Unpacking Our Data Onto The DOM
+const baseURL = "https://swapi.co/api/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+           var dataRow = [];
+           
+           Object.keys(item).forEach(function(key) {
+               var rowData = item[key].toString();
+               var truncatedData = rowData.substring(0, 15);
+               dataRow.push(`<td>${truncatedData}</td>`);
+           });
+           
+           tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
